38b9f7ca8d728e5e97c652f7eb8828d8a675a8eb
Giuseppe Gullo <gfgullo@gmail.com>
2021-03-07 12:43:18 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,10 +20,6 @@ def create_app(config_name):
 
     app = Flask(__name__)
     
-    print(config_name)
-    print("------")
-    print(config)
-
     app.config.from_object(config[config_name])
 
     db.init_app(app)
